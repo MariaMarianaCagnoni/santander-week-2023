018e0ed30aeb609076c78a1451f4c934625af243
mariana <mariamariana.cagnoni@gmail.com>
2023-10-21 17:04:22 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderWeek2023Application.class, args);
+	}
+
+}
