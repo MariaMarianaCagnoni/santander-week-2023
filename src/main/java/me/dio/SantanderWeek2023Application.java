6e0e370a416b794bfd8d74fd650b36875cb5a49a
mariana <mariamariana.cagnoni@gmail.com>
2023-10-22 20:22:00 -0300

    Fix: Problema de CORS corrigido

--- src/main/java/me/dio/SantanderWeek2023Application.java
+++ src/main/java/me/dio/SantanderWeek2023Application.java
@@ -1,13 +1,16 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class SantanderWeek2023Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SantanderWeek2023Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderWeek2023Application.class, args);
+    }
 
 }
